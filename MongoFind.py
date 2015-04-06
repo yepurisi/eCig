078214ce61d164a8de18f2fd03086c7a3719f087
Sid <sidhardha@email.arizona.edu>
2015-04-05 18:08:13 -0700

    Tweet Processing

--- MongoFind.py
+++ MongoFind.py
@@ -2,7 +2,7 @@ __author__ = 'sidhardha'
 
 import pymongo
 from pymongo import MongoClient
-from html.parser import HTMLParser
+import html
 import re
 import pprint
 
@@ -20,15 +20,12 @@ def preProcessTweet(incomingTweet):
     #Replace multiple spaces with single space
     tweet = re.sub(' +', ' ', tweet)
 
-    #Converting the case to be consistent
-    tweet = tweet.lower()
-
     #Removing HTML Characters
-    HTML_Parser = HTMLParser.HTMLParser()
-    tweet = HTML_Parser.unescape(tweet)
+    #HTML_Parser = HTMLParser
+    tweet = html.unescape(tweet)
 
     #Decoding the character set
-    tweet = tweet.decode(encoding='utf-8').encode('ascii', 'ignore')
+    tweet = tweet.encode('ascii', 'ignore').decode(encoding='utf-8')
 
     #Removing URLs in Tweets
     tweet = re.sub('http://[\w\./]*', '', tweet)
@@ -46,6 +43,11 @@ def preProcessTweet(incomingTweet):
     #Remove hash tags
     reformedTweet = reformedTweet.replace('#', '')
 
+    #Parse Multiple words written together
+    reformedTweet = " ".join(re.findall('[A-Z][^A-Z]*', reformedTweet))
+
+    #Converting the case to be consistent
+    reformedTweet = reformedTweet.lower()
 
     return reformedTweet
 
@@ -56,9 +58,7 @@ for eachTweet in tweets_iterator:
     count = count + 1
     print ("*" * 80)
     print (eachTweet['text'])
-    #tweet = " ".join([word for word in re.findall('http\w./', eachTweet['text'])])
-    tweet = re.sub('http://[\w\./]*', '', eachTweet['text'])
-    print (tweet)
+    print (preProcessTweet(eachTweet['text']))
     if count == 30:
         break
 
